4277a0a60a10b329487cda8c1a8e63d5e1f1c526
Liam <Goodlandliam@gmail.com>
2026-01-15 13:48:42 -0800

    Fixed while loop
    Yea guys, I fixed the while loop so it doesent break. Kind of the goat! Best of luck with the 289320832903 bugs this code ads!

--- Liamisawesome.java
+++ Liamisawesome.java
@@ -2,7 +2,7 @@ class Liamisawesome {
     public static void main(String[] args){
 
 
-        while(1 =1){
+        while(1 == 1){
             System.out.println("Liam is hot and cool");
 
             //deez
